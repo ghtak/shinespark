c0284055de0fbfbc1dee368a82242b7c5d4f8e5c
gwonhyeong.tak <gwonhyeong.tak@gmail.com>
2026-02-18 00:01:25 +0900

    feat: initialize application entry point with config and logging, and ignore log files.

--- shinespark_app/src/main.rs
+++ shinespark_app/src/main.rs
@@ -1,7 +1,7 @@
 #[tokio::main]
 async fn main() {
     let config = shinespark::config::AppConfig::new(
-        shinespark::util::workspace_dir().join("configs").join("dev"),
+        shinespark::util::workspace_dir().join("configs")
     )
     .unwrap();
 
